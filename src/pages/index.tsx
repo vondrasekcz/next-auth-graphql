6e3ffb3022c4b020abde0762caba28d988be9a26
Wondry <vondrasekcz@gmail.com>
2021-09-28 23:07:19 +0200

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+    <div>
+
+      <h1>
+        Index Page
+      </h1>
+
+    </div>
+  )
+}
+
+
+export default Home
