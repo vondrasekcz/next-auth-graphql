f4eea4286df4e3397cd9a129fe56e141a8fdce66
Wondry <vondrasekcz@gmail.com>
2021-09-28 23:42:51 +0200

    chore: add lint rules

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
-import type { AppProps } from 'next/app'
+import type { AppProps, } from 'next/app';
 
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps, }: AppProps): JSX.Element => {
+  return <Component {...pageProps} />;
+};
 
 
-export default MyApp
+export default MyApp;
