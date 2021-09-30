70ebb46ae2c3948a7da51e2c4dcd35a94a8ba7aa
Wondry <vondrasekcz@gmail.com>
2021-09-30 22:01:15 +0200

    development query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,29 @@
 import { getLayout, } from '@/components/Layout';
+import { useDialTypesQuery, } from '@/apollo/graphql';
 
 
 const HomePage = (): JSX.Element => {
+  const { loading, error, data, } = useDialTypesQuery();
+  console.log('useDialTypesQuery', loading, error, data);
+
   return (
     <div>
 
       <h1>
         Index Page
       </h1>
+
+      {loading && <div>Loading</div>}
+      {error && <div>{JSON.stringify(error)}</div>}
+      {data && (
+        <ul>
+          {data.dialTypes.map((brand) => (
+            <li key={brand.key}>
+              {brand.value}
+            </li>
+          ))}
+        </ul>
+      )}
 
     </div>
   );
