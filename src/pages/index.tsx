e258628d23206d3676eafadcad3a2821865a57f1
Pavel Vondrasek <vondrasekcz@gmail.com>
2021-11-26 15:44:32 +0100

    test query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,13 @@
 import { getLayout, } from '@/components/Layout';
-import { useDialTypesQuery, } from '@/apollo/graphql';
 
 
 const HomePage = (): JSX.Element => {
-  const { loading, error, data, } = useDialTypesQuery();
-
   return (
     <div>
 
       <h1>
         Index Page
       </h1>
-
-      {loading && <div>Loading</div>}
-      {error && <div>{JSON.stringify(error)}</div>}
-      {data && (
-        <ul>
-          {data.dialTypes.map((brand) => (
-            <li key={brand.key}>
-              {brand.value}
-            </li>
-          ))}
-        </ul>
-      )}
 
     </div>
   );
--- src/pages/private.tsx
+++ src/pages/private.tsx
@@ -1,13 +1,40 @@
+import { CountryCodeEnum, useTestQuery, } from '@/apollo/graphql';
 import { getLayout, } from '@/components/Layout';
 
 
 const PrivatePage = (): JSX.Element => {
+  const { loading, error, data, } = useTestQuery({
+    variables: {
+      countryCode: CountryCodeEnum.Cz,
+    },
+  });
+
+  console.log(loading, error, data?.brands);
+
   return (
     <div>
 
       <h1>
         Private Page
       </h1>
+
+      <div>
+        {loading && (
+          <div>Loading</div>
+        )}
+        {error && (
+          <div>Loading</div>
+        )}
+        {data && (
+          <ul>
+            {data.brands.map((item) => (
+              <li key={item.key}>
+                {item.value}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
 
     </div>
   );
