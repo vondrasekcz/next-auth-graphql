8638eb33b28a0d0ed02be7d2865e0c53db9c3ddc
Pavel Vondrasek <vondrasekcz@gmail.com>
2021-12-16 14:24:58 +0100

    next-translate

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,30 +1,38 @@
 import Link from 'next/link';
+import useTranslation from 'next-translate/useTranslation';
 
 import { useAuthUser, } from '@/libs/auth/hooks/useAuthUser';
 import LoginForm from './LoginForm';
 import UserPanel from './UserPanel';
+import { useRouter } from 'next/router';
 
 
 const Header = (): JSX.Element => {
   const authUser = useAuthUser();
+  const { locale, } = useRouter();
+  const { t, } = useTranslation();
 
 
   return (
     <div>
 
+      <div>
+        {t('common:title')}
+      </div>
+
       <ul>
         <li>
-          <Link href="/">
+          <Link href="/" locale={locale}>
             <a>Home</a>
           </Link>
         </li>
         <li>
-          <Link href="/public">
+          <Link href="/public" locale={locale}>
             <a>Public</a>
           </Link>
         </li>
         <li>
-          <Link href="/private">
+          <Link href="/private" locale={locale}>
             <a>Private</a>
           </Link>
         </li>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
+import useTranslation from 'next-translate/useTranslation';
+
 import { getLayout, } from '@/components/Layout';
 
 
 const HomePage = (): JSX.Element => {
+  const { t, } = useTranslation();
+
+
   return (
     <div>
 
       <h1>
         Index Page
       </h1>
+
+      <div>
+        {t('home:text')}
+      </div>
 
     </div>
   );
